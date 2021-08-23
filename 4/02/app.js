337ed567a585e6f0c3b77c6fed9b68ce3703cb24
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-08-24 01:51:45 +0900

    While

---
+++ 4/02/app.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+const testCaseArray = [];
+for (let i = 0; i < input.length; i++) {
+  const tempValue = input[i].split(' ').map((item) => +item);
+  testCaseArray.push({ A: tempValue[0], B: tempValue[1] });
+}
+
+solution(testCaseArray);
+
+function solution(array) {
+  let checked = true;
+  let i = 0;
+  let answer = '';
+  while (checked) {
+    A = array[i].A;
+    B = array[i].B;
+    i++;
+    if (testCaseArray.length <= i) {
+      break;
+    }
+    answer += A + B + '\n';
+  }
+  console.log(answer);
+}
---
+++ 4/03(*)/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+solution(+input[0]);
+
+function solution(N) {
+  let sum = 0;
+  let num = N;
+  let length = 0;
+  while (true) {
+    length++;
+    sum = Math.floor(num / 10) + (num % 10);
+    num = (num % 10) * 10 + (sum % 10);
+    if (N === num) {
+      break;
+    }
+  }
+  console.log(length);
+}
